e0c7f60d59df29246795113f663ffecb8a6d5426
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-07-18 15:03:29 +0200

    Add Image priority attribute

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,7 +8,7 @@ export default function Card({ srcImg, title, summary, id }) {
 		<Link href={`/${id}`} className={styles.link}>
 			<div className={styles.card}>
                 <div className={styles.img_container}>
-				    <Image src={srcImg} alt={title} className={styles.img} width={290} height={290}/>
+				    <Image src={srcImg} alt={title} className={styles.img} width={290} height={290} priority/>
                 </div>
 				<ItemSummary title={title} summary={summary} page={'home'}/>
 			</div>
