9bd6de3d671af7495f81440968012430fb9fe609
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-07-18 11:13:26 +0200

    Add focus and hover effects

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -15,16 +15,6 @@ export default function Menu() {
     const dispatch = useDispatch();
     const theme = useSelector(selectTheme);
 
-    // function handleKeyDown(e) {
-    //     if(e.target.id === 'first-menu-item' && e.keyCode === 9) {
-    //         const isPressedShift = e.shiftKey;
-
-    //         if(isPressedShift) {
-    //         }
-    //     } else if (e.target.id === 'last-menu-item' && e.keyCode === 9) {
-    //     }
-    // }
-
     return (
         <div
             tabIndex={-1}
@@ -36,7 +26,6 @@ export default function Menu() {
             <div className={styles.icons}>
                 <VscColorMode 
                     className={styles.icon} 
-                    // id='first-menu-item' onKeyDown={handleKeyDown}
                     onClick={() => dispatch(themeActions.toggle())} 
                     role='button' aria-label={`Passer au mode ${theme === 'dark' ? 'light' : 'dark'}`} tabIndex={menu ? 0 : -1} 
                 />
@@ -49,38 +38,43 @@ export default function Menu() {
                 <ul className={styles.text}>
                     <Link 
                         href={'/#projects'} 
+                        className={styles.link}
                         onClick={() => dispatch(menuActions.toggle())} 
                         tabIndex={menu ? 0 : -1}
                     >
-                        <li className={`${styles.li}`}>Projets</li>
+                        <li className={styles.li}>Projets</li>
                     </Link>
                     <Link href={'/contact'} 
+                        className={styles.link}
                         onClick={() => dispatch(menuActions.toggle())}
                         tabIndex={menu ? 0 : -1}
                     >
-                        <li className={`${styles.li}`}>Contact</li>
+                        <li className={styles.li}>Contact</li>
                     </Link>
                     <Link 
+                        className={styles.link}
                         href={'/career'} 
                         onClick={() => dispatch(menuActions.toggle())}  
                         tabIndex={menu ? 0 : -1}
                     >
-                        <li className={`${styles.li}`}>Parcours</li>
+                        <li className={styles.li}>Parcours</li>
                     </Link>
                 </ul>
             </nav>
             <nav className={styles.social}>
-                <a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`} 
+                <a className={styles.link}
+                    href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`} 
                     aria-label='Envoyer un email' tabIndex={menu ? 0 : -1}>
                     <GrMail className={styles.icon}/>
                 </a>
-                <a href="https://github.com/Jeanne4L" target="_blank" 
+                <a className={styles.link}
+                    href="https://github.com/Jeanne4L" target="_blank" 
                     aria-label='Aller sur mon Github' tabIndex={menu ? 0 : -1}>
                     <GrGithub className={styles.icon}/>
                 </a>
-                <a href="https://www.linkedin.com/in/sandra-petereau/" target="_blank" 
+                <a className={styles.link}
+                    href="https://www.linkedin.com/in/sandra-petereau/" target="_blank" 
                     aria-label='Aller sur mon LinkedIn' tabIndex={menu ? 0 : -1}
-                    // id='last-menu-item' onKeyDown={handleKeyDown}
                 >
                     <GrLinkedinOption className={styles.icon}/>
                 </a>
