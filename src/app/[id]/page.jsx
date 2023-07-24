f129889e6365739db7bb2c158b223b0955197f43
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-07-24 14:01:34 +0200

    Fix image link error

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,10 +1,11 @@
+'use client'
 import Image from 'next/image';
 import Tag from '@/components/Tag';
 import Button from '@/components/Button';
-import Link from 'next/link';
 import {notFound} from 'next/navigation';
 import projects from '@/data/projects.json';
 import styles from './page.module.css';
+import { useState } from 'react';
 
 // METADATA
 export let metadata = {
@@ -21,6 +22,10 @@ export default function Project({params}) {
     // FIND PROJECT CORRESPONDING TO PARAMS ID
     const project = projects.find((project) => project.id === params.id);
 
+    const [isOpenedImg, setIsOpenedImg] = useState(false)
+    const [openedImgSrc, setOpenedImgSrc] = useState('')
+    const [openedImgAlt, setOpenedImgAlt] = useState('')
+
     if(!project) {
         notFound()
     } else { 
@@ -28,7 +33,11 @@ export default function Project({params}) {
         metadata.description = `Voici le projet ${project.title} réalisé lors de ma formation chez Openclassrooms. J'y ai appris: ${project.hardSkills.map((skill) => skill)}`;
 
         return (
-            <div className="container">
+            isOpenedImg ? 
+                <div className={styles.overlay} onClick={() => setIsOpenedImg(false)}>
+                    <Image src={openedImgSrc} alt={openedImgAlt} className={styles.full_img} fill priority/>
+                </div>
+            : <div className="container">
                 <div className={styles.introduction}>
                     <Image src={project.mainImg} alt={project.title} width={350} height={350} className={styles.main_img} priority/>
                     <div className={styles.presentation}>
@@ -47,35 +56,50 @@ export default function Project({params}) {
                     </div>
                 </div>
                 <div className={styles.pictures}>
-                    <div className={styles.img_container}>
+                    <div className={styles.picture_container}>
                     <h2 className={styles.img_title}>Home</h2>
-                        <Link href={project.pictures[0]} className={styles.link} aria-label="Afficher l'image de la page principale" >
+                        <div href={project.pictures[0]} className={styles.img_container} aria-label="Afficher l'image de la page principale" >
                             <Image 
                                 src={project.pictures[0]} alt={`Page d'accueil ${project.title}`} 
                                 fill sizes='(max-width: 500px) 100vw, 600px' quality={100}
                                 className={styles.img} priority
+                                onClick={() => {
+                                    setIsOpenedImg(true)
+                                    setOpenedImgSrc(project.pictures[0])
+                                    setOpenedImgAlt(`Page d'accueil ${project.title}`)
+                                }}
                             />  
-                        </Link>
+                        </div>
                     </div>
-                    <div className={styles.img_container}>
+                    <div className={styles.picture_container}>
                         <h2 className={styles.img_title}>Desktop</h2>
-                        <Link href={project.pictures[1]} className={styles.link} aria-label="Afficher l'image du site sur ordinateur">
+                        <div href={project.pictures[1]} className={styles.img_container} aria-label="Afficher l'image du site sur ordinateur">
                             <Image 
                                 src={project.pictures[1]} alt={`Page ${project.title}`} 
                                 fill sizes='(max-width: 500px) 100vw, 600px' quality={100}
                                 className={styles.img} priority
+                                onClick={() => {
+                                    setIsOpenedImg(true)
+                                    setOpenedImgSrc(project.pictures[1])
+                                    setOpenedImgAlt(`Page ${project.title}`)
+                                }}
                             />
-                        </Link>
+                        </div>
                     </div>
-                    <div className={styles.img_container}>
+                    <div className={styles.picture_container}>
                         <h2 className={styles.img_title}>Mobile</h2>
-                        <Link href={project.pictures[2]} className={styles.link} aria-label="Afficher l'image du site sur mobile">
+                        <div href={project.pictures[2]} className={styles.img_container} aria-label="Afficher l'image du site sur mobile">
                             <Image 
                                 src={project.pictures[2]} alt={`${project.title} sur mobile`} 
                                 fill sizes='(max-width: 500px) 100vw, 500px' quality={100}
                                 className={styles.img} priority
+                                onClick={() => {
+                                    setIsOpenedImg(true)
+                                    setOpenedImgSrc(project.pictures[2])
+                                    setOpenedImgAlt(`${project.title} sur mobile`)
+                                }}
                             />
-                        </Link>
+                        </div>
                     </div>
                 </div>
             </div>
