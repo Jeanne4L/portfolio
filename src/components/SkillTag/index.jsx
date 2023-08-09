d3d1db520871d8c5e19d02fd21c0cebe9644ad9f
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-08-09 09:30:06 +0200

    Skills animation

--- src/components/SkillTag/index.jsx
+++ src/components/SkillTag/index.jsx
@@ -1,12 +1,35 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import styles from './index.module.css'
 
 export default function SkillTag({text, children}) {
+    const [isDisplayed, setIsDisplayed] = useState(false);
+
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.5
+    });
+
+    useEffect(() => {
+        if(inView) {
+            setIsDisplayed(true);
+        }
+    }, [inView, isDisplayed]);
+
+    useEffect(() => {
+        return () => {
+            setIsDisplayed(false);
+        };
+    }, []);
+
     return(
         <div className={styles.container}>
-            <div className={styles.circle}>
+            <div className={`${styles.circle} ${isDisplayed ? styles.anim : ''}`} ref={ref}>
                 {children}
             </div>
-            <p className={styles.text}>{text}</p>
+            <p className={`${styles.text} ${isDisplayed ? styles.text_anim : ''}`}>{text}</p>
         </div>
     )
 }
